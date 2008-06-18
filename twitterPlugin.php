89b1d3096718457d70cbe089604272f5acababce
plusnet <plusnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-18 12:46:19 +0000

    New version 1.0.1 taking the changes to the folder reference
    git-svn-id: https://plugins.svn.wordpress.org/twitter-feed/trunk@51047 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitterPlugin.php
+++ twitterPlugin.php
@@ -4,7 +4,7 @@
 Plugin Name: Twitter Feed
 Plugin URI: http://community.plus.net/opensource/twitter-wordpress/
 Description: Selects and displays the latest blog posts from Twitter for specified users.
-Version: 1.0
+Version: 1.0.1
 Author: PlusNet Plc - Developer Responsible: James Tuck (Web Development Team)
 Author URI: http://community.plus.net/opensource/
 */
